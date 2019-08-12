e441707929a5529464dfa652349537f736f9559f
Vuong Chi Son <sonvc@vnpay.vn>
2019-08-12 10:31:10 +0700

    add icon

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -87,7 +87,6 @@ function onMessageReceived(payload) {
     var message = JSON.parse(payload.body);
 
     var messageElement = document.createElement('li');
-
     if (message.type === 'JOIN') {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' joined!';
@@ -113,9 +112,10 @@ function onMessageReceived(payload) {
     var textElement = document.createElement('p');
     var messageText = document.createTextNode(message.content);
     textElement.appendChild(messageText);
-
     messageElement.appendChild(textElement);
-
+    if (message.sender === username){
+        messageElement.classList.add('pad-right')
+    }
     messageArea.appendChild(messageElement);
     messageArea.scrollTop = messageArea.scrollHeight;
 
@@ -123,29 +123,14 @@ function onMessageReceived(payload) {
 
 function onMessageReceivedNoti(payload) {
     var message = JSON.parse(payload.body);
-    if (message.sender == username){
+    if (message.sender === username) {
         return
     }
     if (message.type === 'JOIN') {
-        messageElement.classList.add('event-message');
         message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
-        messageElement.classList.add('event-message');
         message.content = message.sender + ' left!';
     } else {
-        messageElement.classList.add('chat-message');
-
-        var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
-
-        messageElement.appendChild(avatarElement);
-
-        var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
-        usernameElement.appendChild(usernameText);
-        messageElement.appendChild(usernameElement);
     }
     Push.create(message.sender, {
         body: message.content,
